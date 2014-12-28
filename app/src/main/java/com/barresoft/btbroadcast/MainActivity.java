b33e80cb2dda5f553b123a1298bd8328ddbef24b
Cristian <cristianbarreto@gmail.com>
2014-12-28 19:21:07 -0200

    2

--- app/src/main/java/com/barresoft/btbroadcast/MainActivity.java
+++ app/src/main/java/com/barresoft/btbroadcast/MainActivity.java
@@ -2,26 +2,42 @@ package com.barresoft.btbroadcast;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import static java.lang.Thread.sleep;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private static final int REQUEST_ENABLE_BT = 0;
+    private static final UUID MY_UUID = null;
+    private BluetoothSocket mmSocket=null;
+    private BluetoothDevice mmDevice=null;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice>pairedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inicio();
     }
 
 
@@ -47,7 +63,24 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void inicio(){
+    //UTIL------------------------------------------------------------------------------
+    private void msg(String msg){
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void log(String log){
+        try {
+            Log.d(null, log);
+        }catch (Exception e){
+            Log.e(null, "(vacío)");
+        };
+    }
+    //UTIL------------------------------------------------------------------------------
+
+    public void scan(View view){
+        log("SCAN START");
+        Button btnScan = (Button)findViewById(R.id.btnScan);
+        btnScan.setEnabled(false);
+
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             msg("Device does not support Bluetooth");
@@ -56,14 +89,25 @@ public class MainActivity extends ActionBarActivity {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }else{ //is enabled
-                //WHILE !!!!!!!!!!!!!
-                Util.beep();
+                msg("BT Already on..." + "\n" +
+                     "SCAN START");
+
+                pairedDevices = BA.getBondedDevices();
+
+                ArrayList list = new ArrayList();
+                for(BluetoothDevice bt : pairedDevices){
+                    list.add(bt.getName());
+                    msg(bt.getName());
+                }
+
                 // Create a BroadcastReceiver for ACTION_FOUND
-                final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+                /*final BroadcastReceiver mReceiver = new BroadcastReceiver() {
                     public void onReceive(Context context, Intent intent) {
+                        log("1");
                         String action = intent.getAction();
                         // When discovery finds a device
                         if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                            log("2");
                             // Get the BluetoothDevice object from the Intent
                             BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                             // Add the name and address to an array adapter to show in a ListView
@@ -71,16 +115,62 @@ public class MainActivity extends ActionBarActivity {
                             msg(device.getName() + "\n" + device.getAddress());
                         }
                     }
+                    public void onDestroy(){
+                        super.onDestroy();
+                        Log.d("onDestroy called!", "onDestroy called!");
+                        unregisterReceiver(mReceiver);
+                    }
                 };
+
                 // Register the BroadcastReceiver
                 IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
                 registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-                Util.beep();
+                log(mReceiver.getResultData());
+                */
             }
         }
+        log("SCAN STOP");
+        //btnScan.setEnabled(true);
     }
 
-    private void msg(String msg){
-        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    private void ConnectThread(BluetoothDevice device) {
+        // Use a temporary object that is later assigned to mmSocket,
+        // because mmSocket is final
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+
+        // Get a BluetoothSocket to connect with the given BluetoothDevice
+        try {
+            // MY_UUID is the app's UUID string, also used by the server code
+            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) { }
+        mmSocket = tmp;
+    }
+
+    public void run(BluetoothAdapter mBluetoothAdapter) {
+        // Cancel discovery because it will slow down the connection
+        mBluetoothAdapter.cancelDiscovery();
+
+        try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and get out
+            try {
+                mmSocket.close();
+            } catch (IOException closeException) { }
+            return;
+        }
+
+        // Do work to manage the connection (in a separate thread)
+        //manageConnectedSocket(mmSocket);
+    }
+
+    /** Will cancel an in-progress connection, and close the socket */
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
     }
 }
